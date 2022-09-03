78161b124663d0cd6aede193af7834efe68b2a01
JanickSpielmann <56891695+JanickSpielmann@users.noreply.github.com>
2022-09-04 00:42:47 +0200

    Initial upload

---
+++ KaraProjekt_V1.1 - M319/Kara/Form1.SteuerungKara.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace KaraProjekt
+{
+    //cKara Version 1.1 by Barbara Bielawski
+    public partial class Form1 : Form
+    {
+        private void Main(string args)
+        {
+            //Hier die Anweisungen notieren
+
+
+           
+            //Programmende: nächste Zeile bitte nicht löschen
+            MessageBox.Show("Programmcode ausgeführt!");
+        }
+
+        //Hier können Sie eigene Methoden einfügen
+
+
+    }
+}
